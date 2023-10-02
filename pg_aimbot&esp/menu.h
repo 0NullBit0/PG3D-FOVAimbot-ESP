b4335590e53dbe150a9161cdb121a81c6352d276
0NullBit0 <131892100+0NullBit0@users.noreply.github.com>
2023-10-02 12:48:01 +0200

    sc

--- pg_aimbot&esp/menu.h
+++ pg_aimbot&esp/menu.h
@@ -16,14 +16,14 @@ void draw_ESP(){
 
             if (enemylistESP[i].enemy) {
                 ESP::DrawLine(ImVec2((float) glWidth / 2, 0.0f),
-                                ImVec2((float) glWidth - (glWidth - enemylistESP[i].pos.X),
-                                        ((float) glHeight - enemylistESP[i].pos.Y - 10.0f)),
+                                ImVec2((float) glWidth - (glWidth - enemylistESP[i].screenPos.X),
+                                        ((float) glHeight - enemylistESP[i].screenPos.Y - 10.0f)),
                                 ImVec4(1.0f, 0.0f, 0.0f, 1.0f), 3.0f);
                 
             } else {
                 ESP::DrawLine(ImVec2((float) glWidth / 2, 0.0f),
-                                ImVec2((float) glWidth - (glWidth - enemies[i].pos.X),
-                                        ((float) glHeight - enemies[i].pos.Y - 10.0f)),
+                                ImVec2((float) glWidth - (glWidth - enemylistESP[i].screenPos.X),
+                                        ((float) glHeight - enemylistESP[i].screenPos.Y - 10.0f)),
                                 ImVec4(0.0f, 1.0f, 0.0f, 1.0f), 3.0f);
                 
             }
